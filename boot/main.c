24b8c45a34a797b3ed86736e09db00f7a81ad8f9
Sehwani2 <hyn0216388@gmail.com>
2026-04-28 17:14:40 +0900

    printf

--- boot/main.c
+++ boot/main.c
@@ -1,8 +1,10 @@
 #include "stdint.h"
 #include "HalUart.h"
+
 #include "stdio.h"
 
 static void Hw_init(void);
+static void Printf_test(void);
 
 void main(void)
 {
@@ -17,6 +19,8 @@ void main(void)
 
     putstr("Hello World!\n");
 
+    Printf_test();
+
     i = 100;
     while(i--)
     {
@@ -28,4 +32,18 @@ void main(void)
 static void Hw_init(void)
 {
     Hal_uart_init();
+}
+
+
+static void Printf_test(void)
+{
+    char* str = "printf pointer test";
+    char* nullptr = 0;
+    uint32_t i = 5;
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str);
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
 }
