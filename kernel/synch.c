b7284f41ef4f87a253c9833be78ce2c1cec0ccda
Sehwani2 <hyn0216388@gmail.com>
2026-05-04 19:04:45 +0900

    mutex

--- kernel/synch.c
+++ kernel/synch.c
@@ -10,11 +10,17 @@
 static int32_t sSemMax;
 static int32_t sSem;
 
+KernelMutext_t sMutex;
+
+
 void Kernel_sem_init(int32_t max)
 {
     sSemMax = (max <= 0) ? DEF_SEM_MAX : max;
+    sSemMax = (max >= DEF_SEM_MAX) ? DEF_SEM_MAX : max;
+
     sSem = sSemMax;
 }
+
 
 bool Kernel_sem_test(void)
 {
@@ -30,10 +36,38 @@ bool Kernel_sem_test(void)
 
 void Kernel_sem_release(void)
 {
+    sSem++;
+
     if (sSem >= sSemMax)
     {
         sSem = sSemMax;
     }
+}
 
-    sSem++;
+void Kernel_mutex_init(void)
+{
+    sMutex.owner = 0;
+    sMutex.lock = false;
+}
+
+bool Kernel_mutex_lock(uint32_t owner)
+{
+    if (sMutex.lock)
+    {
+        return false;
+    }
+
+    sMutex.owner = owner;
+    sMutex.lock = true;
+    return true;
+}
+
+bool Kernel_mutex_unlock(uint32_t owner)
+{
+    if (owner == sMutex.owner)
+    {
+        sMutex.lock = false;
+        return true;
+    }
+    return false;
 }
